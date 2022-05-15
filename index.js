fcaad8562ee8aee2aff0f9eeeb2cddeab4f1aa3c
wellitonscheer <wellitonscheer@gmail.com>
2022-05-14 21:58:18 -0300

    estado botao

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+const blue = "rgb(0, 0, 255)";
+const red = "rgb(255, 0, 0)";
+const ligado = "Ligado"
+const desligado = "Desligado"
+
+function onToggle() {
+    return function() {
+        const element = $(this);
+        const newColor = element.css("background-color") === red ? blue : red;
+        const estado = element.css("background-color") === red ? ligado : desligado;
+        element.css("background-color", newColor);
+        element.text(estado);
+    };
+}
+
 $(document).ready(function(){
-
-    $("#fridge").click(function(){
-        $("#btn_fridge").toggleClass("control fridge_desligado");
-    });
-
+    $("#fridge").click(onToggle());
 });
