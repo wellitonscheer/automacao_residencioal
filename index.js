38e94f9f6c6f0b3aa8bde0db9bae50896b61aa02
wellitonscheer <wellitonscheer@gmail.com>
2022-05-14 23:10:55 -0300

    garagem

--- index.js
+++ index.js
@@ -1,51 +1,33 @@
 const blue = "rgb(0, 0, 255)";
 const red = "rgb(255, 0, 0)";
-const ligado = "Ligado"
-const desligado = "Desligado"
+const ligado = "Ligado";
+const desligado = "Desligado";
+const aberto = "Aberto";
+const fechado = "Fechado";
 
-function onToggle() {
+function onToggle(abertoFechado) {
     return function() {
         const element = $(this);
         const newColor = element.css("background-color") === red ? blue : red;
-        const estado = element.css("background-color") === red ? ligado : desligado;
+        let estado;
+        if(abertoFechado){
+            estado = element.css("background-color") === red ? aberto : fechado;
+        }else{
+            estado = element.css("background-color") === red ? ligado : desligado;
+        }
         element.css("background-color", newColor);
-        element.text(estado);
+        element.children("span").eq(0).text(estado);
     };
 }
 
 $(document).ready(function(){
     $("#fridge").click(onToggle());
-});
-
-$(document).ready(function(){
     $("#tv-quarto").click(onToggle());
-});
-
-$(document).ready(function(){
     $("#tv-sala").click(onToggle());
-});
-
-$(document).ready(function(){
     $("#tv-quarto-visita").click(onToggle());
-});
-
-$(document).ready(function(){
-    $("#porta-garagem").click(onToggle());
-});
-
-$(document).ready(function(){
+    $("#porta-garagem").click(onToggle(true));
     $("#luz-quarto-esquerda").click(onToggle());
-});
-
-$(document).ready(function(){
     $("#luz-quarto-direita").click(onToggle());
-});
-
-$(document).ready(function(){
     $("#luz-quarto-visita-direita").click(onToggle());
-});
-
-$(document).ready(function(){
     $("#luz-quarto-visita-esquerda").click(onToggle());
 });
-
